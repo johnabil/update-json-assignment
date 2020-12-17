19051a9a872578c2b9853ec89374b9195ca8bd4f
John Nabil <johnnabil85@gmail.com>
2020-12-17 23:16:37 +0200

    finishing synchronize api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/api/update/:key', function (Request, Response) {
+  const fs = require('fs');
+
+  const key = Request.params.key;
+  const value = Request.body.value;
+  let playerData = require('../player-data.json');
+
+  if (playerData.hasOwnProperty(key))
+    playerData[key] = value;
+
+  let data = JSON.stringify(playerData);
+  fs.writeFileSync(`${Request.app.get('root')}/player-data.json`, data);
+  return Response.status(200).json(playerData);
 });
 
 module.exports = router;
